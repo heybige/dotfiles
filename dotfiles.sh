0ffd759b0d4b815407400e828695c915ae9eb921
Eric Anderson <eric.anderson@realpage.com>
2015-09-30 10:44:52 -0700

    change dotfiles to .dotfiles

--- dotfiles.sh
+++ dotfiles.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-if [ -d ~/dotfiles ]; then
-    # Check if ~/dotfiles is empty or has stuff in it
-    echo "~/dotfiles exists - aborting.."
+if [ -d ~/.dotfiles ]; then
+    # Check if ~/.dotfiles is empty or has stuff in it
+    echo "~/.dotfiles exists - aborting.."
     exit
 fi
 
@@ -11,22 +11,22 @@ if [ ! -x "$path_to_git" ] ; then
     sudo apt-get -y install git
 fi
 
-git clone https://github.com/heybige/dotfiles.git ~/dotfiles
+git clone https://github.com/heybige/dotfiles.git ~/.dotfiles
 
 if [ ! -f ~/.bash_aliases ]; then
-    ln -s ~/dotfiles/.bash_aliases
+    ln -s ~/.dotfiles/.bash_aliases
 fi
 
 if [ ! -f ~/.exrc ]; then
-    ln -s ~/dotfiles/.exrc
+    ln -s ~/.dotfiles/.exrc
 fi
 
 if [ ! -f ~/.gitignore ]; then
-    ln -s ~/dotfiles/.gitignore
+    ln -s ~/.dotfiles/.gitignore
 fi
 
 if [ ! -f ~/.vimrc ]; then
-    ln -s ~/dotfiles/.vimrc
+    ln -s ~/.dotfiles/.vimrc
 fi
 
 # reload .bashrc to load .bash_aliases
